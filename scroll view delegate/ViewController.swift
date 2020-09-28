cd24aa7787f695553e1937192d9973f32ea469ca
Lydia-Lu-code <guipopo5@gmail.com>
2020-09-28 12:53:15 +0800

    scroll view delegate -end

--- scroll view delegate/ViewController.swift
+++ scroll view delegate/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet var photos: [UIImageView]!
+    @IBOutlet weak var scrollControl: UIScrollView!
+    @IBOutlet weak var pagesControl: UIPageControl!
+    @IBOutlet var viewOutlet: UIView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        scrollControl.delegate = self
+        pagesControl.numberOfPages = 5
+        pagesControl.currentPage = 0
+        
+        
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let currentPage = Int(scrollControl.contentOffset.x / scrollControl.frame.size.width)
+        
+        pagesControl.currentPage = currentPage
     }
 
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        for viewOutlet in scrollControl.subviews{
+            if viewOutlet.isKind(of: UIView.self){
+                return viewOutlet
+            }
+        }
+        return nil
+    }
 
 }
 
